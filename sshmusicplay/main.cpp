cc5673cd04653a6ccd51d9ce4c8e81755bfe122e
Mika Tammi <mikatammi@gmail.com>
2013-02-17 22:12:17 +0200

    Added added libavcodec and libavformat includes to main.cpp

--- sshmusicplay/main.cpp
+++ sshmusicplay/main.cpp
@@ -1,6 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
+
+extern "C"
+{
+#ifndef INT64_C
+#define INT64_C(c) (c ## LL)
+#define UINT64_C(c) (c ## ULL)
+#endif
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +20,11 @@ int main(int argc, char *argv[])
     MainWindow mainWindow;
     mainWindow.setOrientation(MainWindow::ScreenOrientationAuto);
     mainWindow.showExpanded();
+
+    qDebug() << "Initializing libavcodec/libavformat";
+
+    // Register all formats and codecs for libavcodec/libavformat
+    av_register_all();
 
     return app.exec();
 }
