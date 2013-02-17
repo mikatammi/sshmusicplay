ac3b6b3484da7a48031d99d69dd3a13f32a2a447
Mika Tammi <mikatammi@gmail.com>
2013-02-18 00:45:16 +0200

    Implemented most functionality of SSHfile class

--- sshmusicplay/sshfile.cpp
+++ sshmusicplay/sshfile.cpp
@@ -1,6 +1,87 @@
 #include "sshfile.hpp"
 
-SSHFile::SSHFile(QObject *parent) :
-    QObject(parent)
+extern "C"
 {
+#include <fcntl.h> // Needed for O_RDONLY
+}
+
+SSHFile::SSHFile(sftp_session session,
+                 QObject *parent) :
+    QObject(parent),
+    file_opened_(false),
+    sftp_session_(session)
+{
+}
+
+SSHFile::~SSHFile()
+{
+    // Close file if it is open
+    if (file_opened_)
+    {
+        close();
+    }
+}
+
+bool SSHFile::open(QString filename)
+{
+    Q_ASSERT(!file_opened_);
+
+    // Try to open file
+    sftp_file_ = sftp_open(sftp_session_, filename.toStdString().c_str(),
+                           O_RDONLY, 0);
+
+    // Check if file didn't open
+    if (sftp_file_ == NULL)
+    {
+        file_opened_ = false;
+        return false;
+    }
+
+    file_opened_ = true;
+    return true;
+}
+
+void SSHFile::close()
+{
+    // Close file if it has been opened
+    if (file_opened_)
+    {
+        // Close file
+        sftp_close(sftp_file_);
+
+        // Set file open status to closed
+        file_opened_ = false;
+    }
+}
+
+ssize_t SSHFile::read(void *buf, size_t count)
+{
+    Q_ASSERT(file_opened_);
+
+    // Read file
+    return sftp_read(sftp_file_, buf, count);
+}
+
+void SSHFile::rewind()
+{
+    Q_ASSERT(file_opened_);
+
+    // Rewind file
+    sftp_rewind(sftp_file_);
+}
+
+bool SSHFile::seek(uint32_t new_offset)
+{
+    Q_ASSERT(file_opened_);
+
+    // Seek file
+    return sftp_seek(sftp_file_, new_offset) == 0;
+}
+
+unsigned long SSHFile::tell()
+{
+    Q_ASSERT(file_opened_);
+
+    // Tell file position
+    return sftp_tell(sftp_file_);
 }
--- sshmusicplay/sshfile.hpp
+++ sshmusicplay/sshfile.hpp
@@ -3,15 +3,62 @@
 
 #include <QObject>
 
+// Include libssh SFTP header
+extern "C"
+{
+#include <libssh/sftp.h>
+}
+
 class SSHFile : public QObject
 {
     Q_OBJECT
 public:
-    explicit SSHFile(QObject *parent = 0);
-    
+    /// Constructor
+    explicit SSHFile(sftp_session session,
+                     QObject *parent = 0);
+
+    /// Destructor
+    virtual ~SSHFile();
+
+    /// Open file. After this call, read functions become accessible.
+    /// @return True if success, false if not.
+    bool open(QString filename);
+
+    /// Close file. After this call, file is not accessible anymore.
+    void close();
+
+    /// Tells if file is open.
+    /// @return True if file is open, false if not.
+    bool is_open();
+
+    /// Read count bytes to buffer buf.
+    /// @param buf Pointer to buffer
+    /// @param count Count of bytes to read
+    /// @return Number of bytes written to buffer. Less than 0 on error.
+    ssize_t read(void *buf, size_t count);
+
+    /// Rewind to beginning of file
+    void rewind();
+
+    /// Seek file to offset
+    /// @return True if success, false if fails
+    bool seek(uint32_t new_offset);
+
+    /// Tells current position in file
+    /// @return Current position in file
+    unsigned long tell();
+
 signals:
     
 public slots:
+
+private:
+    // Variable which holds if file is open or not
+    bool file_opened_;
+
+    // Structs for sftp file and session (libssh)
+    sftp_file sftp_file_;
+    sftp_session sftp_session_;
     
 };
 
