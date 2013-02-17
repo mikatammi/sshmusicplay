053046cc7efd1d3353204c5820f60c95cdd9a23c
Mika Tammi <mikatammi@gmail.com>
2013-02-17 22:11:39 +0200

    Added some test SSH connection code to main window

--- sshmusicplay/mainwindow.cpp
+++ sshmusicplay/mainwindow.cpp
@@ -3,8 +3,14 @@
 
 #include <QCoreApplication>
 #include <QMessageBox>
+#include <QDebug>
 
+extern "C"
+{
 #include <libssh/libssh.h>
+#include <libssh/sftp.h>
+}
+
 #include <cstdlib>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -65,6 +71,8 @@ void MainWindow::showExpanded()
 
 void MainWindow::doConnect()
 {
+    // TODO: After testing, move this stuff to ssh session class
+
     ui->lblStatus->setText("Connecting...");
 
     ssh_session my_ssh_session = ssh_new();
@@ -95,12 +103,128 @@ void MainWindow::doConnect()
         return;
     }
 
-    ui->lblStatus->setText("Success! Disconnecting.");
 
+    // TODO: Authenticate server
     // TODO: Show question regarding correct host key
     //QMessageBox::question(this, "HahaaTitle", "HahaaTeksti",
     //    QMessageBox::Yes || QMessageBox::No || QMessageBox::Cancel);
 
+    /*
+    rc = ssh_userauth_password(my_ssh_session,
+                               ui->txtUsername->text().toStdString().c_str(),
+                               ui->txtPassword->text().toStdString().c_str());
+    if (rc != SSH_AUTH_SUCCESS)
+    {
+        ui->lblStatus->setText(QString("Authentication error:")
+                               + ssh_get_error(my_ssh_session));
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        return;
+    }
+    */
+
+    rc = ssh_userauth_kbdint(my_ssh_session,
+                       ui->txtUsername->text().toStdString().c_str(),
+                       NULL);
+    while (rc == SSH_AUTH_INFO)
+    {
+        const char *name;
+        const char *instruction;
+        int nprompts, iprompt = 0;
+
+        name = ssh_userauth_kbdint_getname(my_ssh_session);
+        instruction = ssh_userauth_kbdint_getinstruction(my_ssh_session);
+        nprompts = ssh_userauth_kbdint_getnprompts(my_ssh_session);
+
+        qDebug() << "Name Instruction Nprompts";
+        qDebug() << name << instruction << nprompts;
+
+        if (nprompts > 0)
+        {
+            const char *prompt;
+            char echo;
+
+            prompt = ssh_userauth_kbdint_getprompt(my_ssh_session, 0, &echo);
+
+            qDebug() << prompt;
+
+            if(QString(prompt) == "Password: ")
+            {
+                if (ssh_userauth_kbdint_setanswer(
+                            my_ssh_session, iprompt,
+                            ui->txtPassword->text().toStdString().c_str()) < 0)
+                {
+                    ssh_disconnect(my_ssh_session);
+                    ssh_free(my_ssh_session);
+                    return;
+                }
+            }
+            else
+            {
+                ssh_disconnect(my_ssh_session);
+                ssh_free(my_ssh_session);
+                return;
+            }
+        }
+
+        rc = ssh_userauth_kbdint(my_ssh_session,
+                           ui->txtUsername->text().toStdString().c_str(),
+                           NULL);
+    }
+
+    // TODO: Maybe some other stuff
+    sftp_session sftp;
+    sftp = sftp_new(my_ssh_session);
+    if (sftp == NULL)
+    {
+        ui->lblStatus->setText(QString("Error allocating SFTP session.")
+                               + ssh_get_error(my_ssh_session));
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        return;
+    }
+
+    rc = sftp_init(sftp);
+    if (rc != SSH_OK)
+    {
+        ui->lblStatus->setText(QString("Error initializing SFTP session.")
+                               + ssh_get_error(my_ssh_session));
+        sftp_free(sftp);
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        return;
+    }
+
+    sftp_dir dir;
+    sftp_attributes attributes;
+
+    dir = sftp_opendir(sftp, "/tmp");
+    if (!dir)
+    {
+        ui->lblStatus->setText(QString("Directory not opened: ")
+                               + ssh_get_error(my_ssh_session));
+        sftp_free(sftp);
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        return;
+    }
+
+    attributes = sftp_readdir(sftp, dir);
+    attributes = sftp_readdir(sftp, dir);
+    attributes = sftp_readdir(sftp, dir);
+    attributes = sftp_readdir(sftp, dir);
+
+    ui->lblStatus->setText(QString("Success! Disconnecting.")
+                           + attributes->name);
+
+    sftp_closedir(dir);
+    if (rc != SSH_OK)
+    {
+        ui->lblStatus->setText(QString("Cannot close dir.")
+                               + ssh_get_error(my_ssh_session));
+    }
+
+    sftp_free(sftp);
     ssh_disconnect(my_ssh_session);
     ssh_free(my_ssh_session);
 }
